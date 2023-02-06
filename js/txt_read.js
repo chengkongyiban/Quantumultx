43e3607efd3b0b62d3745ce523bdc2ba31821a59
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-02-06 18:45:33 +0800

    更新 txt_read.js

--- js/txt_read.js
+++ js/txt_read.js
@@ -15,7 +15,8 @@ const isStashiOS = 'undefined' !== typeof $environment && $environment['stash-ve
 const isSurgeiOS = 'undefined' !== typeof $environment && $environment['surge-version'];
 const isShadowrocket = 'undefined' !== typeof $rocket;
 const isLooniOS = 'undefined' != typeof $loon && /iPhone/.test($loon);
-
+const isQuanX = 'undefined' !== typeof $task;
+	
 let req = $request.url.replace(/\.t_read\.txt$/,'');
 
 !(async () => {
@@ -23,7 +24,9 @@ let req = $request.url.replace(/\.t_read\.txt$/,'');
 //判断是否断网
 if(body == null){if(isSurgeiOS || isStashiOS){
 	$notification.post("重写转换：未获取到body","请检查网络及节点是否畅通","认为是bug?点击通知反馈",{url:"https://t.me/zhangpeifu"})
- $done({ response: { status: 404 ,body:{} } });}else{$notification.post("重写转换：未获取到body","请检查网络及节点是否畅通","认为是bug?点击通知反馈","https://t.me/zhangpeifu")
+ $done({ response: { status: 404 ,body:{} } });}else if (isQuanX){
+	$notify("重写转换：未获取到body", "请检查网络及节点是否畅通", "认为是bug?点击通知反馈","https://t.me/zhangpeifu");
+}else{$notification.post("重写转换：未获取到body","请检查网络及节点是否畅通","认为是bug?点击通知反馈","https://t.me/zhangpeifu")
  $done({ response: { status: 404 ,body:{} } });
 }//识别客户端通知
 }else{
