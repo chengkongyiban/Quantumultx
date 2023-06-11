f75d61434f98d3c11dfb85659df42725c33b283f
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-06-12 00:58:51 +0800

    Create i4adBlock.js

---
+++ js/i4adBlock.js
@@ -0,0 +1,17 @@
+/*
+^http:\/\/d\.image\.i4\.cn\/adInfo\/ url reject-img
+^https?:\/\/list-app-m\.i4\.cn\/get(HotSearch|App)List\.xhtml url script-response-body 
+hostname = list-app-m.i4.cn
+*/
+let i = JSON.parse($response.body);
+
+  i.adli=[]
+
+  i.list= []
+
+  i.ad = []
+
+  i.app=[]
+
+$done({body: JSON.stringify(i)});
+
