40f56d1843ec56f784fda684804f9a6d4aa880e0
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-06-12 01:08:45 +0800

    更新 i4Ad.js

--- js/i4Ad.js
+++ js/i4Ad.js
@@ -1,6 +1,6 @@
 /*
 author=keywos
-^http:\/\/d\.image\.i4\.cn\/adInfo\/ url reject-img
+^http:\/\/d\.image\.i4\.cn\/adInfo\/ url reject
 ^https?:\/\/list-app-m\.i4\.cn\/get(HotSearch|App)List\.xhtml url script-response-body https://github.com/chengkongyiban/Quantumultx/raw/main/js/i4Ad.js 
 hostname = list-app-m.i4.cn
 */
