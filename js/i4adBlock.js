2b4d6d7408d9d63a3d6fdb04850a56a0333a9a28
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-06-12 00:59:51 +0800

    Update i4adBlock.js

--- js/i4adBlock.js
+++ js/i4adBlock.js
@@ -1,6 +1,6 @@
 /*
 ^http:\/\/d\.image\.i4\.cn\/adInfo\/ url reject-img
-^https?:\/\/list-app-m\.i4\.cn\/get(HotSearch|App)List\.xhtml url script-response-body 
+^https?:\/\/list-app-m\.i4\.cn\/get(HotSearch|App)List\.xhtml url script-response-body https://github.com/chengkongyiban/Quantumultx/raw/main/js/i4adBlock.js 
 hostname = list-app-m.i4.cn
 */
 let i = JSON.parse($response.body);
