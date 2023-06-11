6bd6668ee4ed4ae4ecab42d63e2f9e08e57cb23f
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-06-12 01:12:12 +0800

    Update and rename i4Ad.js to i4Ad1.js

--- js/i4Ad1.js
+++ js/i4Ad1.js
@@ -1,7 +1,7 @@
 /*
 author=keywos
 ^http:\/\/d\.image\.i4\.cn\/adInfo\/ url reject
-^https?:\/\/list-app-m\.i4\.cn\/get(HotSearch|App)List\.xhtml url script-response-body https://github.com/chengkongyiban/Quantumultx/raw/main/js/i4Ad.js 
+^https?:\/\/list-app-m\.i4\.cn\/get(HotSearch|App)List\.xhtml url script-response-body https://github.com/chengkongyiban/Quantumultx/raw/main/js/i4Ad1.js 
 hostname = list-app-m.i4.cn
 */
 let i = JSON.parse($response.body);
