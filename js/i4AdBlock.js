254cad0478c1b65189074e41d39e607b334522b8
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-06-12 11:02:28 +0800

    更新 i4AdBlock.js

--- js/i4AdBlock.js
+++ js/i4AdBlock.js
@@ -5,15 +5,12 @@ author=keywos
 ^https?:\/\/list-app-m\.i4\.cn\/get(HotSearch|App)List\.xhtml url script-response-body https://github.com/chengkongyiban/Quantumultx/raw/main/js/i4AdBlock.js 
 hostname = list-app-m.i4.cn
 */
+let u = $request.url;
 let i = JSON.parse($response.body);
-
-  i.adli=[]
-
-  i.list= []
-
-  i.ad = []
-
-  i.app=[]
-
+if (u.includes("list-app-m.i4.cn")) {
+  i.app && (i.app=[])
+}
+	i.adli && (i.adli=[])
+	i.list && (i.list= [])
+	i.ad && (i.ad = []);
 $done({body: JSON.stringify(i)});
-
